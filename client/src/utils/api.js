aa960bf55a815ad017c3e7f3fb3aeba363d45c2b
Bhaskar Jyoti Kherkatary <kherkatarybhaskar@gmail.com>
2021-09-08 10:54:49 +0530

    READY FOR DEPLOYMENT

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,17 +1,17 @@
 import axios from 'axios';
 
-const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
-  headers: {
-    'Content-Type': 'application/json'
-  },
-  mode: 'cors',
-});
 // const api = axios.create({
-//   baseURL: '/api',
+//   baseURL: 'http://localhost:5000/api',
 //   headers: {
 //     'Content-Type': 'application/json'
-//   }
+//   },
+//   mode: 'cors',
 // });
+const api = axios.create({
+  baseURL: '/api',
+  headers: {
+    'Content-Type': 'application/json'
+  }
+});
 
 export default api;
